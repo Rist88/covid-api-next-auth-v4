bdd7c2ede838b92977778953e7242d7715fbe832
Andrew Rudenko <andrew.rudenko9@gmail.com>
2022-06-29 21:42:25 +0300

    add authOptions export

--- auth/[...nextauth].ts
+++ auth/[...nextauth].ts
@@ -12,59 +12,59 @@ import { PrismaClient } from "@prisma/client";
 // // const prisma = new PrismaClient()
 // // For more information on each option (and a full list of options) go to
 // // https://next-auth.js.org/configuration/options
-// export const authOptions: NextAuthOptions = {
-//   // https://next-auth.js.org/configuration/providers/oauth
-//   // adapter: PrismaAdapter(prisma),
-//   providers: [
-//     /* EmailProvider({
-//          server: process.env.EMAIL_SERVER,
-//          from: process.env.EMAIL_FROM,
-//        }),
-//     // Temporarily removing the Apple provider from the demo site as the
-//     // callback URL for it needs updating due to Vercel changing domains
+export const authOptions: NextAuthOptions = {
+  // https://next-auth.js.org/configuration/providers/oauth
+  // adapter: PrismaAdapter(prisma),
+  providers: [
+    /* EmailProvider({
+         server: process.env.EMAIL_SERVER,
+         from: process.env.EMAIL_FROM,
+       }),
+    // Temporarily removing the Apple provider from the demo site as the
+    // callback URL for it needs updating due to Vercel changing domains
 
-//     Providers.Apple({
-//       clientId: process.env.APPLE_ID,
-//       clientSecret: {
-//         appleId: process.env.APPLE_ID,
-//         teamId: process.env.APPLE_TEAM_ID,
-//         privateKey: process.env.APPLE_PRIVATE_KEY,
-//         keyId: process.env.APPLE_KEY_ID,
-//       },
-//     }),
-//     */
-//     FacebookProvider({
-//       clientId: process.env.FACEBOOK_ID,
-//       clientSecret: process.env.FACEBOOK_SECRET,
-//     }),
-//     GithubProvider({
-//       clientId: process.env.GITHUB_ID,
-//       clientSecret: process.env.GITHUB_SECRET,
-//     }),
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_ID,
-//       clientSecret: process.env.GOOGLE_SECRET,
-//     }),
-//     TwitterProvider({
-//       clientId: process.env.TWITTER_ID,
-//       clientSecret: process.env.TWITTER_SECRET,
-//     }),
-//     Auth0Provider({
-//       clientId: process.env.AUTH0_ID,
-//       clientSecret: process.env.AUTH0_SECRET,
-//       issuer: process.env.AUTH0_ISSUER,
-//     }),
-//   ],
-//   theme: {
-//     colorScheme: "light",
-//   },
-//   callbacks: {
-//     async jwt({ token }) {
-//       token.userRole = "admin";
-//       return token;
-//     },
-//   },
-// };
+    Providers.Apple({
+      clientId: process.env.APPLE_ID,
+      clientSecret: {
+        appleId: process.env.APPLE_ID,
+        teamId: process.env.APPLE_TEAM_ID,
+        privateKey: process.env.APPLE_PRIVATE_KEY,
+        keyId: process.env.APPLE_KEY_ID,
+      },
+    }),
+    */
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_ID,
+      clientSecret: process.env.AUTH0_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
+    }),
+  ],
+  theme: {
+    colorScheme: "light",
+  },
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin";
+      return token;
+    },
+  },
+};
 
 // // export default NextAuth(authOptions);
-export {};
+export default {};
